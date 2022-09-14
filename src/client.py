79920889f5d4584824d9fc91be3b8e75e61d5bcc
JohannUM <72266031+JohannUM@users.noreply.github.com>
2022-09-14 15:12:54 +0200

    Added JSON

--- src/client.py
+++ src/client.py
@@ -1,5 +1,6 @@
-from ctypes import FormatError
+import json
 import socket
+import sys
 
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -19,6 +20,22 @@ def send_message(message):
     client.send(msg)
     print(client.recv(128).decode(FORMAT))
     
-input = str(input())
-send_message(input)
+id = input("Enter your ID: ")
+password = input("Enter your password: ")
+actions = []
+print("Enter your actions (Q to end): ")
+while True:
+    action = input()
+    if action == "Q":
+        break
+    actions.append(action)
+delay = int(input("Enter the dealy between actions: "))
+
+data = {"id":id, "password":password, "server":{"ip":SERVER, "port":PORT}, "actions":{"delay":delay, "steps":actions}}
+json_data = json.dumps(data)
+
+send_message(json_data)
 send_message(DISCONNECT)
+#input = str(input())
+#send_message(input)
+#send_message(DISCONNECT)
